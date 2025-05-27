2ce85f31caa78b1d1d7aa0e02faab5e9e172cce4
lixandro_beltre <lixan@BLACKPANTHER>
2025-05-27 19:59:12 -0400

    Add project files.

---
+++ Backend/BookLibrary.API/Data/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookLibrary.API.Data.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int TotalCopies { get; set; }
+        public int CopiesInUse { get; set; }
+        public string? Type { get; set; }
+        public string? Isbn { get; set; }
+        public string? Category { get; set; }
+    }
+}
