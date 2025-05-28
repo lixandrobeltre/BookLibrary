6af484f9f874c425bbd355be88baa91c419f4028
Lixandro Beltre <lixandro.beltre@hotmail.com>
2025-05-28 07:36:23 -0400

    refactoring: change db credential to secret approach

--- Backend/BookLibrary.API/Program.cs
+++ Backend/BookLibrary.API/Program.cs
@@ -2,13 +2,25 @@ using BookLibrary.API.Controllers;
 using BookLibrary.API.Data;
 using BookLibrary.API.Interfaces;
 using BookLibrary.API.Services;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BLContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("BLDbConnection")));
+
+var connectionStringBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("BLDbConnection"))
+{
+    UserID = builder.Configuration["bl_db_user"],
+    Password = builder.Configuration["bl_db_pass"]
+};
+
+builder.Services.AddDbContext<BLContext>(op => op.UseSqlServer(connectionStringBuilder.ConnectionString));
 builder.Services.AddScoped<IBookServices, BookServices>();
 builder.Services.AddEndpointsApiExplorer();
+
+var dbUser = Environment.GetEnvironmentVariable("bl_db_user");
+
+//builder.Configuration.AddUserSecrets<Program>();
 
 var FE_CORS = "fe_cors_local"; //for testing FE local
 
